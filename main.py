29e9c44ce74fef797f0815a7bb489ef43014e71e
damienbeecroft <damienbeecroft@gmail.com>
2024-02-26 15:51:49 -0800

    I added code that loads the pretrained ResNet50 and Distillbert architectures. The Distillbert set up has not been tested since we do not have a text dataset loaded to the GitHub.

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
 import argparse
 from torch import nn, optim
 from sklearn.metrics import confusion_matrix
+from torchvision.models import resnet50, ResNet50_Weights
+from transformers import DistilBertTokenizer, DistilBertModel
 
-from model import FeedforwardNeuralNet
 from data import MNISTDataset, CIFAR10Dataset
-from trainer import Trainer
+from trainer import Trainer_Image, Trainer_Text
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--epochs', type=int, default=1)
 parser.add_argument('--lr', type=float, default=0.01)
 parser.add_argument('--train_batch_size', type=int, default=32)
 parser.add_argument('--eval_batch_size', type=int, default=32)
+parser.add_argument('--model',type=str,default='resnet')
 args = parser.parse_args()
 
 print("Running with the following arguments: ", args)
 
-dataset = MNISTDataset(args.train_batch_size, args.eval_batch_size)
-model = FeedforwardNeuralNet(dataset.input_size, dataset.output_size)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=args.lr)
+if args.model == 'resnet':
+    dataset = CIFAR10Dataset(args.train_batch_size, args.eval_batch_size)
+    model = resnet50(weights=ResNet50_Weights.DEFAULT)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr)
 
-trainer = Trainer(model, dataset, criterion, optimizer)
-trainer.train(args.epochs)
-trainer.evaluate()
-conf_matrix = confusion_matrix(trainer.true_labels, trainer.predictions)
-print("Confusion Matrix =\n", conf_matrix)
+    trainer = Trainer_Image(model, dataset, criterion, optimizer)
+    trainer.train(args.epochs)
+    trainer.evaluate()
+    conf_matrix = confusion_matrix(trainer.true_labels, trainer.predictions)
+    print("Confusion Matrix =\n", conf_matrix)
+elif args.model == 'distillbert':
+    dataset = None # TODO
+    tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+    model = DistilBertModel.from_pretrained('distilbert-base-uncased')    
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr)
+
+    trainer = Trainer_Image(model, dataset, criterion, optimizer)
+    trainer.train(args.epochs)
+    trainer.evaluate()
+    conf_matrix = confusion_matrix(trainer.true_labels, trainer.predictions)
+    print("Confusion Matrix =\n", conf_matrix)
+
+    # text = "Hello, world!"
+    # encoded_input = tokenizer(text, return_tensors='pt')
+    # output = model(**encoded_input)
+else:
+    print('Input model not recognized :/')
--- trainer.py
+++ trainer.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from typing import Tuple, List, Any
 
-class Trainer:
+class Trainer_Image:
     def __init__(self, model: nn.Module, dataset: Dataset, criterion: Any, optimizer: optim.Optimizer) -> None:
         self.model = model
         self.dataset = dataset
@@ -28,8 +28,6 @@ class Trainer:
         for epoch in range(epochs):
             self.model.train()
             for inputs, labels in self.dataset.train_loader:
-                inputs = inputs.view(inputs.shape[0], -1)
-
                 if torch.cuda.is_available():
                     inputs = inputs.cuda()
                     labels = labels.cuda()
@@ -55,8 +53,6 @@ class Trainer:
         self.eval_loss_history.append((iteration_count, eval_loss))
         self.accuracies.append(accuracy)
         print(f"Iteration {iteration_count}, Training loss: {loss.item():.4f}, Eval loss: {eval_loss:.4f}, Accuracy: {accuracy:.4f}")
-
-
         self.plot_losses()
         self.plot_accuracy()
 
@@ -68,12 +64,108 @@ class Trainer:
 
         with torch.no_grad():
             for inputs, labels in self.dataset.test_loader:
-                inputs = inputs.view(inputs.shape[0], -1)
+                if torch.cuda.is_available():
+                    inputs = inputs.cuda()
+                    labels = labels.cuda()
+                output = self.model(inputs)
+                loss = self.criterion(output, labels)
+                total_loss += loss.item()
+
+                _, predicted = torch.max(output, 1)
+                self.predictions.extend(predicted.cpu().numpy())
+                self.true_labels.extend(labels.cpu().numpy())
+
+        average_loss = total_loss / len(self.dataset.test_loader)
+        accuracy = accuracy_score(self.true_labels, self.predictions)
+        return average_loss, accuracy
+
+    def plot_losses(self) -> None:
+        plt.figure(figsize=(12, 6))
+        eval_iterations, eval_losses = zip(*self.eval_loss_history)
+        plt.plot(self.all_train_losses, label='Training Loss', alpha=0.5)
+        plt.plot(eval_iterations, eval_losses, label='Evaluation Loss', marker='o', linestyle='-', linewidth=2)
+        plt.xlabel('Iteration')
+        plt.ylabel('Loss')
+        plt.title('Training and Evaluation Loss Over Iterations')
+        plt.legend()
+        plt.show()
+
+    def plot_accuracy(self) -> None:
+        if self.accuracies:  # Ensure there are accuracy data points to plot
+            eval_iterations, _ = zip(*self.eval_loss_history)
+            plt.figure(figsize=(12, 6))
+            plt.plot(eval_iterations, self.accuracies, label='Accuracy', marker='o', linestyle='-', linewidth=2)
+            plt.xlabel('Iteration')
+            plt.ylabel('Accuracy')
+            plt.title('Model Accuracy Over Iterations')
+            plt.legend()
+            plt.show()
+
+
+class Trainer_Text:
+    def __init__(self, model: nn.Module, tokenizer: nn.Module, dataset: Dataset, criterion: Any, optimizer: optim.Optimizer) -> None:
+        self.model = model
+        self.dataset = dataset
+        self.criterion = criterion
+        self.optimizer = optimizer
+
+        self.all_train_losses: List[float] = []
+        self.eval_loss_history: List[Tuple[int, float]] = []  # (iteration, eval_loss)
+        self.accuracies: List[float] = []
+        self.predictions: List[int] = []
+        self.true_labels: List[int] = []
+
+        if torch.cuda.is_available():
+            self.model.cuda()
+
+    def train(self, epochs: int, eval_every_n_iterations: int = 100) -> None:
+        iteration_count = 0
+
+        for epoch in range(epochs):
+            self.model.train()
+            for inputs, labels in self.dataset.train_loader:
                 if torch.cuda.is_available():
                     inputs = inputs.cuda()
                     labels = labels.cuda()
 
-                output = self.model(inputs)
+                self.optimizer.zero_grad()
+                encoded_inputs = self.tokenizer(inputs, return_tensors='pt')
+                output = self.model(encoded_inputs)
+                loss = self.criterion(output, labels)
+                loss.backward()
+                self.optimizer.step()
+
+                self.all_train_losses.append(loss.item())
+                iteration_count += 1
+
+                if iteration_count % eval_every_n_iterations == 0:
+                    eval_loss, accuracy = self.evaluate()
+                    self.eval_loss_history.append((iteration_count, eval_loss))
+                    self.accuracies.append(accuracy)
+                    print(f"Iteration {iteration_count}, Training loss: {loss.item():.4f}, Eval loss: {eval_loss:.4f}, Accuracy: {accuracy:.4f}")
+
+    
+        # Evaluate at the end of training
+        eval_loss, accuracy = self.evaluate()
+        self.eval_loss_history.append((iteration_count, eval_loss))
+        self.accuracies.append(accuracy)
+        print(f"Iteration {iteration_count}, Training loss: {loss.item():.4f}, Eval loss: {eval_loss:.4f}, Accuracy: {accuracy:.4f}")
+        self.plot_losses()
+        self.plot_accuracy()
+
+    def evaluate(self) -> Tuple[float, float]:
+        self.model.eval()
+        total_loss = 0
+        self.predictions = []  # Reset predictions
+        self.true_labels = []  # Reset true labels
+
+        with torch.no_grad():
+            for inputs, labels in self.dataset.test_loader:
+                if torch.cuda.is_available():
+                    inputs = inputs.cuda()
+                    labels = labels.cuda()
+                encoded_inputs = self.tokenizer(inputs, return_tensors='pt')
+                output = self.model(encoded_inputs)
                 loss = self.criterion(output, labels)
                 total_loss += loss.item()
 
