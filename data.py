ac20f35626aafef175943b92263ab42d15ef119d
Nahom Alemseged <70569094+NahomAlemseged@users.noreply.github.com>
2024-02-26 23:54:07 -0800

    Update data.py
    I was not sure if I could update the Dataset function instead of data_load

--- data.py
+++ data.py
@@ -1,7 +1,10 @@
 import torch
 from torchvision import datasets, transforms
 from dataclasses import dataclass
-
+from torch.utils.data import dataset, DataLoader
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+from torch.nn.utils.rnn import pad_sequence
 @dataclass
 class Dataset:
     train_loader: torch.utils.data.DataLoader
@@ -31,3 +34,40 @@ class CIFAR10Dataset(Dataset):
         self.train_loader = torch.utils.data.DataLoader(trainset, batch_size=trian_batch_size, shuffle=True)
         testset = datasets.CIFAR10('~/.pytorch/CIFAR10_data/', download=True, train=False, transform=transform)
         self.test_loader = torch.utils.data.DataLoader(testset, batch_size=eval_batch_size, shuffle=True)
+
+class data_load(Dataset):
+    def __init__(self, data, tokenizer):
+        self.data = data
+        self.tokenizer = tokenizer
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        text = self.data[idx]
+        encoding = self.tokenizer.encode(text)
+        return torch.tensor(encoding.ids)
+class data_handle:
+    def __init__(self, train_data, test_data, tokenizer, batch_size=1, shuffle_train=True, shuffle_test=False):
+        self.train_dataset = data_load(train_data, tokenizer)
+        self.test_dataset = data_load(test_data, tokenizer)
+        self.batch_size = batch_size
+        self.shuffle_train = shuffle_train
+        self.shuffle_test = shuffle_test
+        self.train_loader = DataLoader(
+            dataset=self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=self.shuffle_train
+            )
+
+        self.test_loader = DataLoader(
+            dataset=self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=self.shuffle_test
+            )
+    def get_train_loader(self):
+        return self.train_loader
+
+    def get_test_loader(self):
+        return self.test_loader
+    def numericalize(text):
+        return [vocab[token] for token in tokenizer(text)]
